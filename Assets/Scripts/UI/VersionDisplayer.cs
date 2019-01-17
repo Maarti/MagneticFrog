4fb6a811e30886676da4ccd00dabf770dfbaa800
Maarti <bryan.martinet@gmail.com>
2019-01-16 22:57:40 -0500

    Version displayed in settings menu

---
+++ Assets/Scripts/UI/VersionDisplayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class VersionDisplayer : MonoBehaviour {
+
+    void Start() {
+        GetComponent<TextMeshProUGUI>().text = "Version " + Application.version.ToString();
+    }
+}
