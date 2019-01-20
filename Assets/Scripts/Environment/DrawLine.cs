359a1951a03a016d2e74bbe7f78d18bac58f15e5
Maarti <bryan.martinet@gmail.com>
2019-01-20 16:14:16 -0500

    Magnet modeled and implement with rope physics

---
+++ Assets/Scripts/Environment/DrawLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DrawLine : MonoBehaviour {
+
+    [Tooltip("The objects to draw the line between")]
+    [SerializeField] GameObject[] targets;
+    private LineRenderer lineRenderer;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = targets.Length;
+        SetPositions();
+    }
+
+    void LateUpdate() {
+        SetPositions();
+    }
+
+    void SetPositions() {
+        for (int i = 0; i < targets.Length; i++) {
+            lineRenderer.SetPosition(i, targets[i].transform.position);
+        }
+    }
+}
