93f737eceece78aaabd44c12993fcf12c1d9eb47
Maarti <bryan.martinet@gmail.com>
2019-02-02 18:41:27 -0500

    URL opener

---
+++ Assets/Scripts/UI/UrlManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class UrlManager : MonoBehaviour {
+
+    public void OpenLink(string url) {
+#if UNITY_ANDROID
+        if (url == "URL_PLAY_STORE")
+            url = "URL_PLAY_STORE_ANDROID";
+#endif
+        Debug.Log("Opening url " + url);
+        Application.OpenURL(url);
+    }
+}
