788c1ce8e08dd9f2e59f7fd6d4968856fa0ab245
Maarti <bryan.martinet@gmail.com>
2019-02-02 13:03:59 -0500

    New buttons + audio mute/unmute

---
+++ Assets/Scripts/UI/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour {
+    [SerializeField] Image audioButtonImage;
+    [SerializeField] Sprite muteImage;
+    [SerializeField] Sprite unmuteImage;
+
+    public void ToggleMuteAudio() {
+        if (isMuted())
+            Unmute();
+        else
+            Mute();
+    }
+
+    bool isMuted() {
+        return AudioListener.volume == 0f;
+    }
+
+    public void Mute() {
+        AudioListener.volume = 0f;
+        audioButtonImage.sprite = muteImage;
+    }
+
+    public void Unmute() {
+        AudioListener.volume = 1f;
+        audioButtonImage.sprite = unmuteImage;
+    }
+}
