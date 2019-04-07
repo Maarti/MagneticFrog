b6d3b2c9927bb1645d2f66ed04b75af67dad2d0c
Maarti <bryan.martinet@gmail.com>
2019-04-06 22:01:04 -0400

    ground design + sorting layers + vertical parallax

---
+++ Assets/Scripts/Environment/VerticalParallax.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class VerticalParallax : MonoBehaviour
+{
+
+    [SerializeField] Camera cam;
+    [Tooltip("When the camera reaches this Y coordinate, the element will have reach his target point")]
+    [SerializeField] float targetCameraY = 200f;
+    [Tooltip("Y coordinate the element will move to")]
+    [SerializeField] float targetLocalY = -3f;
+
+    float startLocalY = 0f;
+    float startCameraY = 0f;
+    float totalCameraDistanceToTravel = 200f;
+
+
+    void Start()
+    {
+        startLocalY = transform.localPosition.y;
+        startCameraY = cam.transform.position.y;
+        totalCameraDistanceToTravel = targetCameraY - startCameraY;
+    }
+        
+    void Update()
+    {
+        Vector3 newPosition = transform.localPosition;
+        float newY = Mathf.Lerp(startLocalY, targetLocalY, (cam.transform.position.y-startCameraY)/totalCameraDistanceToTravel);
+        newPosition.y = newY;
+        transform.localPosition = newPosition;
+    }
+}
