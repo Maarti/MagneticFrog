7c8e45b2b29e9f5450177740da0e7ea9a49b0d17
Maarti <bryan.martinet@gmail.com>
2019-04-22 13:38:15 -0400

    Jump tutorial animation

---
+++ Assets/Scripts/GameMecanics/TutorialAnimatorOrchester.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TutorialAnimatorOrchester : MonoBehaviour {
+
+    Animator anim;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TriggerTouchAnimation() {
+        anim.SetTrigger("touch");
+    }
+}
