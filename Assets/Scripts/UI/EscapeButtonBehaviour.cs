297bf119b435f62db3e9a9eee07573459f36eb95
Maarti <bryan.martinet@gmail.com>
2019-04-28 13:11:03 -0400

    Android back button behaviour implemented

---
+++ Assets/Scripts/UI/EscapeButtonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class EscapeButtonBehaviour : MonoBehaviour {
+    [Tooltip("If one of the children is active when Escape is pressed, this button will not be triggered")]
+    [SerializeField] GameObject[] childrenButtons;    
+    GraphicRaycaster graphicRaycaster ;
+    Button backButton;
+
+    void Start() {
+        backButton = GetComponent<Button>();
+        graphicRaycaster = GetComponentInParent<GraphicRaycaster>();
+    }
+
+    void Update() {
+        // If Android back button is pressed
+        if (Input.GetKeyDown(KeyCode.Escape) && backButton != null && graphicRaycaster!=null && graphicRaycaster.isActiveAndEnabled) {
+            bool activeChildExists = false;
+            foreach (GameObject child in childrenButtons) {
+                if (child.activeInHierarchy) activeChildExists = true;
+            }
+            if (!activeChildExists)
+                backButton.onClick.Invoke();
+        }
+    }
+}
