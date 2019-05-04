1257dedd21ad92807d89e8068b90e93393d8529e
Maarti <bryan.martinet@gmail.com>
2019-05-04 18:09:44 -0400

    Delay between ads

--- Assets/Scripts/GoogleServices/PlayGamesActivator.cs
+++ Assets/Scripts/GoogleServices/PlayGamesActivator.cs
@@ -9,7 +9,7 @@ public class PlayGamesActivator : MonoBehaviour {
 
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
             // enables saving game progress.
-            .EnableSavedGames()
+         //   .EnableSavedGames()
             // registers a callback to handle game invitations received while the game is not running.
             //    .WithInvitationDelegate(< callback method >)
             // registers a callback for turn based match notifications received while the
@@ -17,13 +17,13 @@ public class PlayGamesActivator : MonoBehaviour {
             //     .WithMatchDelegate(< callback method >)
             // requests the email address of the player be available.
             // Will bring up a prompt for consent.
-            .RequestEmail()
+        //    .RequestEmail()
             // requests a server auth code be generated so it can be passed to an
             //  associated back end server application and exchanged for an OAuth token.
-            .RequestServerAuthCode(false)
+   //         .RequestServerAuthCode(false)
             // requests an ID token be generated.  This OAuth token can be used to
             //  identify the player to other services such as Firebase.
-            .RequestIdToken()
+      //      .RequestIdToken()
             .Build();
 
         PlayGamesPlatform.InitializeInstance(config);
