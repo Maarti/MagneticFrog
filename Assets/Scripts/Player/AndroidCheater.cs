fc9c6e6378077187b48b49998dcb17a81840f1ef
Maarti <bryan.martinet@gmail.com>
2019-05-05 20:40:44 -0400

    Stun animation + flip animation + new idle animation

---
+++ Assets/Scripts/Player/AndroidCheater.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class AndroidCheater : MonoBehaviour
+{
+    int nbClick = 0;
+
+    public void Start() {
+        Debug.LogFormat("DONT FORGET to remove Cheater script on {0}", gameObject.name);
+    }
+
+    public void Money() {
+        nbClick++;
+        if (nbClick >= 10) {
+            ApplicationController.ac.UpdateCoins(99999);
+            nbClick = 0;
+        }
+
+    }
+}
