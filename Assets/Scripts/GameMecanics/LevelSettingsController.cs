f071cca0ea50522bd52b50600a73459747eb4206
Maarti <bryan.martinet@gmail.com>
2019-05-28 23:24:35 -0400

    WIP Spawning bursts

--- Assets/Scripts/GameMecanics/LevelSettingsController.cs
+++ Assets/Scripts/GameMecanics/LevelSettingsController.cs
@@ -4,11 +4,13 @@ public class LevelSettingsController : MonoBehaviour {
 
     [SerializeField] MeterCounter meterCounter;
     public static LevelSettings currentLevelSettings;
+    public static int nextBurst = 0;
     public delegate void LevelSettingsChange(LevelSettings levelSettings);
     public static event LevelSettingsChange OnLevelSettingsChange;
 
     void OnEnable() {
         currentLevelSettings = LevelSettings.GetLevelSettingsScore(LevelSettings.LEVEL_10_SCORE);
+        nextBurst = 0;
         if (OnLevelSettingsChange != null)
             OnLevelSettingsChange(currentLevelSettings);
     }
