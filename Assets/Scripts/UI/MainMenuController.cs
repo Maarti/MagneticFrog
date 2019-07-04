7aec29a4b592f92dd11f09bc138fb629c091cd98
Maarti <bryan.martinet@gmail.com>
2019-07-04 18:27:08 -0400

    Bugfix menu transition

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -3,6 +3,8 @@ using TMPro;
 using DG.Tweening;
 using System;
 
+enum CurrentMenu { Main, Settings, Character }
+
 [RequireComponent(typeof(Animator))]
 public class MainMenuController : MonoBehaviour {
 
@@ -24,6 +26,7 @@ public class MainMenuController : MonoBehaviour {
 
     Animator mainMenuAnim;
     bool isStarted = false;
+    CurrentMenu currentMenu = CurrentMenu.Main;
 
     private void Start() {
         mainMenuAnim = GetComponent<Animator>();
@@ -74,37 +77,41 @@ public class MainMenuController : MonoBehaviour {
     }
 
     public void DisplayCharacterMenu() {
+        currentMenu = CurrentMenu.Character;
         characterMenu.localScale = new Vector3(0f, 0f, 1f);
         characterMenu.DOScale(Vector3.one, .25f);
-        mainMenuCanvasGroup.DOFade(0f, .25f).OnComplete(() => mainMenuCanvasGroup.transform.parent.gameObject.SetActive(false));
+        mainMenuCanvasGroup.DOFade(0f, .25f).OnComplete(() => mainMenuCanvasGroup.transform.parent.gameObject.SetActive(currentMenu == CurrentMenu.Main));
         //   gameTitleCanvasGroup.DOFade(0f, .25f);
         scoreShellAnim.SetBool("isDisplayed", false);
     }
 
     public void HideCharacterMenu() {
+        currentMenu = CurrentMenu.Main;
         characterMenu.localScale = Vector3.one;
         gameTitleCanvasGroup.alpha = 1f;
         mainMenuCanvasGroup.transform.parent.gameObject.SetActive(true);
         mainMenuCanvasGroup.alpha = 0f;
         mainMenuCanvasGroup.DOFade(1f, 1f);
         scoreShellAnim.SetBool("isDisplayed", true);
-        characterMenu.DOScale(new Vector3(0f, 0f, 1f), .25f).OnComplete(() => characterMenu.transform.parent.gameObject.SetActive(false));
+        characterMenu.DOScale(new Vector3(0f, 0f, 1f), .25f).OnComplete(() => characterMenu.transform.parent.gameObject.SetActive(currentMenu == CurrentMenu.Character));
     }
 
     public void DisplaySettingsMenu() {
+        currentMenu = CurrentMenu.Settings;
         gameTitleCanvasGroup.DOFade(0f, .5f);
-        mainMenuCanvasGroup.DOFade(0f, .5f).OnComplete(() => mainMenuCanvasGroup.transform.parent.gameObject.SetActive(false));
+        mainMenuCanvasGroup.DOFade(0f, .5f).OnComplete(() => mainMenuCanvasGroup.transform.parent.gameObject.SetActive(currentMenu == CurrentMenu.Main));
         settingsCanvasGroup.gameObject.SetActive(true);
         settingsCanvasGroup.alpha = 0f;
         settingsCanvasGroup.DOFade(1f, .5f);
     }
 
     public void HideSettingsMenu() {
+        currentMenu = CurrentMenu.Main;
         gameTitleCanvasGroup.DOFade(1f, .5f);
         mainMenuCanvasGroup.transform.parent.gameObject.SetActive(true);
         mainMenuCanvasGroup.alpha = 0f;
         mainMenuCanvasGroup.DOFade(1f, .5f);
-        settingsCanvasGroup.DOFade(0f, .5f).OnComplete(() => settingsCanvasGroup.gameObject.SetActive(false));
+        settingsCanvasGroup.DOFade(0f, .5f).OnComplete(() => settingsCanvasGroup.gameObject.SetActive(currentMenu == CurrentMenu.Settings));
     }
 
     //----------------------
