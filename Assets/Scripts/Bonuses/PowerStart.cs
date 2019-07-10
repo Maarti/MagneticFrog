c6f464a28703000353b1873669bace553b145579
Maarti <bryan.martinet@gmail.com>
2019-07-09 22:50:30 -0400

    Premium implemented

--- Assets/Scripts/Bonuses/PowerStart.cs
+++ Assets/Scripts/Bonuses/PowerStart.cs
@@ -2,7 +2,7 @@
 
 public class PowerStart : MonoBehaviour {
     [SerializeField] ScreenTransition screenTransition;
-    [SerializeField] PlayerController playerCtrlr;    
+    [SerializeField] PlayerController playerCtrlr;
     bool isStarted = false;
 
     public void Start() {
@@ -32,7 +32,7 @@ public class PowerStart : MonoBehaviour {
     }
 
     public void BeginPowerStart() {
-        if (ApplicationController.ac.PlayerData.nbPowerStart <= 0) return;
+        if (!IsPowerStartAvailable()) return;
         ApplicationController.ac.UpdatePowerStart(-1);
         gameObject.SetActive(false);
         playerCtrlr.jumpCtrlr.isInvincible = true;
@@ -43,15 +43,15 @@ public class PowerStart : MonoBehaviour {
         Invoke("RemovePlayerInvincibility", 3f);
         playerCtrlr.jumpCtrlr.isInvincible = true;
         Vector3 pos = playerCtrlr.transform.position;
-        float targetDistance =  ApplicationController.ac.PlayerData.bestScore * Random.Range(.33f,.45f);
+        float targetDistance = ApplicationController.ac.PlayerData.bestScore * Random.Range(.33f, .45f);
         LevelSettingsController.SkipBurstsUntil(targetDistance);
-        pos.y = Mathf.Max(pos.y,targetDistance);
+        pos.y = Mathf.Max(pos.y, targetDistance);
         playerCtrlr.transform.position = pos;
     }
 
     bool IsPowerStartAvailable() {
         if (ApplicationController.ac.PlayerData.isPremium) return true;
-       // System.TimeSpan timeSinceLastActivation = System.DateTime.Now.Subtract(ApplicationController.ac.PlayerData.bonusesActivationTime);
+        // System.TimeSpan timeSinceLastActivation = System.DateTime.Now.Subtract(ApplicationController.ac.PlayerData.bonusesActivationTime);
         return (ApplicationController.ac.PlayerData.bestScore >= 100f) && (ApplicationController.ac.PlayerData.nbPowerStart > 0);
     }
 
