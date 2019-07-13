81af2e4669650f4a58e4251f6241552b83c5a5e8
Maarti <bryan.martinet@gmail.com>
2019-07-13 00:36:47 -0400

    ResetTutorial button activator

---
+++ Assets/Scripts/UI/ButtonActivator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum ButtonType { ResetTuto, IAP_Premium, IAP_Frogbot }
+
+public class ButtonActivator : MonoBehaviour {
+
+    [SerializeField] ButtonType type;
+    Button button;
+    bool isStarted = false;
+
+    private void Start() {
+        button = GetComponent<Button>();
+        isStarted = true;
+    }
+
+    private void OnEnable() {
+        if (!isStarted) Start();
+        RefreshUI();
+    }
+
+    private void RefreshUI() {
+        bool enable = true;
+        switch (type) {
+            case ButtonType.ResetTuto:
+                enable = ApplicationController.ac.PlayerData.isTutorialDone;
+                break;
+        }
+        button.interactable = enable;
+    }
+
+
+}
