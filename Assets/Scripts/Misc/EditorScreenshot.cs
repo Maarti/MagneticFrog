3872c888a4d3f0bf8a8ee92c39b00c9d89b83ff2
Maarti <bryan.martinet@gmail.com>
2019-07-13 02:01:05 -0400

    Screenshot

---
+++ Assets/Scripts/Misc/EditorScreenshot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorScreenshot : MonoBehaviour {
+    [MenuItem("Screenshot/Take screenshot")]
+    static void Screenshot() {
+        ScreenCapture.CaptureScreenshot("screenshot.png");
+    }
+}
