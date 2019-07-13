45d820a7346009dda0aee3ba282cdee67bbf0205
Maarti <bryan.martinet@gmail.com>
2019-07-12 23:58:15 -0400

    Premium label in the version

--- Assets/Scripts/UI/VersionDisplayer.cs
+++ Assets/Scripts/UI/VersionDisplayer.cs
@@ -4,7 +4,12 @@ using TMPro;
 [RequireComponent(typeof(TextMeshProUGUI))]
 public class VersionDisplayer : MonoBehaviour {
 
-    void Start() {
-        GetComponent<TextMeshProUGUI>().text = "Version " + Application.version.ToString();
+    private void OnEnable() {
+        RefreshUI();
+    }
+
+    private void RefreshUI() {
+        string premiumString = (ApplicationController.ac.PlayerData.isPremium) ? " - Premium" : "";
+        GetComponent<TextMeshProUGUI>().text = "Version " + Application.version.ToString() + premiumString;
     }
 }
