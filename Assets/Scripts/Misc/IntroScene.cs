5b1df2bf9abce7bda729aeb0e8796e79f3e50228
Maarti <bryan.martinet@gmail.com>
2019-08-19 21:32:28 -0400

    Skip intro scene

--- Assets/Scripts/Misc/IntroScene.cs
+++ Assets/Scripts/Misc/IntroScene.cs
@@ -2,12 +2,17 @@
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
-public class IntroScene : MonoBehaviour
-{
+public class IntroScene : MonoBehaviour {
     [SerializeField] PlayableDirector director;
 
     void OnEnable() {
         director.stopped += OnPlayableDirectorStopped;
+    }
+
+    private void Update() {
+        if (Input.touchCount > 0) {
+            SceneManager.LoadScene("main_scene", LoadSceneMode.Single);
+        }
     }
 
     void OnPlayableDirectorStopped(PlayableDirector aDirector) {
