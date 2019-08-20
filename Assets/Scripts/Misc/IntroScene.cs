1d2344b2f4ad1339fcf2a071733433cfef5e115d
Maarti <bryan.martinet@gmail.com>
2019-08-20 19:14:32 -0400

    Bugfix: Skip intro scene

--- Assets/Scripts/Misc/IntroScene.cs
+++ Assets/Scripts/Misc/IntroScene.cs
@@ -10,8 +10,12 @@ public class IntroScene : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.touchCount > 0) {
-            SceneManager.LoadScene("main_scene", LoadSceneMode.Single);
+#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+        if (Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Escape)){
+#else
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetMouseButtonDown(0)) {
+#endif        
+            director.Stop();
         }
     }
 
